35a2f86001846ff1d14128953314a8740bcec2d9
Oleksandr Yefremov <ext-oleksandr.yefremov@here.com>
2022-04-09 20:43:26 +0300

    Improve implementation

--- priority_queue.h
+++ priority_queue.h
@@ -6,58 +6,90 @@
 
 template <typename T>
 class priority_queue {
-    size_t m_size = 0;
     std::vector<T> m_data;
 
     void make_heap_impl(){
-        
-        for (int i=m_size / 2 - 1; i>=0; i--) {
+        for (int i=size() / 2 - 1; i>=0; i--) {
             int largest_index = i;
             int left_child_index = 2*i + 1;
             int right_child_index = 2*i + 2;
-            if (left_child_index < m_size && m_data[left_child_index] > m_data[largest_index]) {
+            if (left_child_index < size() && m_data[left_child_index] > m_data[largest_index]) {
                 largest_index = left_child_index;
             }
-            if (right_child_index < m_size && m_data[right_child_index] > m_data[largest_index]) {
+            if (right_child_index < size() && m_data[right_child_index] > m_data[largest_index]) {
                 largest_index = right_child_index;
             }
             if (largest_index != i) {
-                std::swap(m_data[i], m_data[largest_index]);
+                using std::swap;
+                swap(m_data[i], m_data[largest_index]);
+                propagate_heap_down(largest_index);
+            }
+        }
+    }
+
+    void propagate_heap_up(int i){
+        while (i > 0) {
+            int parent_index = (i - 1) / 2;
+            if (m_data[i] > m_data[parent_index]) {
+                using std::swap;
+                swap(m_data[i], m_data[parent_index]);
+                i = parent_index;
+            } else {
+                break;
+            }
+        }
+    }
+
+    void propagate_heap_down(int i){
+        while (i < size()) {
+            int largest_index = i;
+            int left_child_index = 2*i + 1;
+            int right_child_index = 2*i + 2;
+            if (left_child_index < size() && m_data[left_child_index] > m_data[largest_index]) {
+                largest_index = left_child_index;
+            }
+            if (right_child_index < size() && m_data[right_child_index] > m_data[largest_index]) {
+                largest_index = right_child_index;
+            }
+            if (largest_index != i) {
+                using std::swap;
+                swap(m_data[i], m_data[largest_index]);
+                i = largest_index;
+            } else {
+                break;
             }
         }
     }
 
 public:
     priority_queue(size_t capacity) {
-        m_data.resize(capacity);
+        m_data.reserve(capacity);
     }
 
     template<typename U>
     void push(U&& value) {
         assert(!is_full());
-        m_data[m_size] = std::forward<U>(value);
-        m_size++;
-        make_heap_impl();
+        m_data.push_back( std::forward<U>(value) );
+        propagate_heap_up(size() - 1);
     }
 
-    T pop() {
+    void pop() {
         assert(has_items());
-        std::swap(m_data[0], m_data[m_size - 1]);
-        m_size--;
-        make_heap_impl();
-        return std::move( m_data[m_size] );
+        m_data.front() = std::move(m_data.back());
+        m_data.pop_back();
+        propagate_heap_down(0);
     }
 
     const T& top() const {
         assert(has_items());
-        return m_data[0];
+        return m_data.front();
     }
 
     template<typename U>
     void replace_top(U&& value) {
         assert(has_items());
-        m_data[0] = std::forward<U>(value);
-        make_heap_impl();
+        m_data.front() = std::forward<U>(value);
+        propagate_heap_down(0);
     }
 
     // Push new item is capacity allows it
@@ -83,7 +115,6 @@ public:
         // First chunk of the input can fit in the queue
         auto first_chunk_size = std::min(input_size, capacity());
         m_data.assign(begin, begin + first_chunk_size);
-        m_size = first_chunk_size;
         make_heap_impl();
 
         // Process the rest of the input
@@ -94,19 +125,19 @@ public:
     }
 
     bool empty() const {
-        return m_size == 0;
+        return m_data.empty();
     }
 
     bool has_items() const {
-        return m_size > 0;
+        return !empty();
     }
 
     bool is_full() const {
-        return m_size == m_data.capacity();
+        return size() == capacity();
     }
 
     size_t size() const {
-        return m_size;
+        return m_data.size();
     }
 
     size_t capacity() const {
