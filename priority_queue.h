2c56c5b0c5b7e8dbfe49070717b2234c330da68f
Oleksandr Yefremov <ext-oleksandr.yefremov@here.com>
2022-04-09 16:09:24 +0300

    Add more operations

--- priority_queue.h
+++ priority_queue.h
@@ -17,9 +17,10 @@ public:
         m_data.resize(capacity);
     }
 
-    void push(T&& value) {
+    template<typename U>
+    void push(U&& value) {
         assert(!is_full());
-        m_data[m_new_element_index] = std::forward<T>(value);
+        m_data[m_new_element_index] = std::forward<U>(value);
         m_new_element_index++;
         m_size++;
         std::make_heap(m_data.data(), m_data.data() + m_size);
@@ -29,6 +30,7 @@ public:
         assert(has_items());
         std::swap(m_data[0], m_data[m_size - 1]);
         m_size--;
+        std::make_heap(m_data.data(), m_data.data() + m_size);
         return std::move( m_data[m_size] );
     }
 
@@ -37,10 +39,44 @@ public:
         return m_data[m_top_index];
     }
 
-    void replace_top(T value) {
+    template<typename U>
+    void replace_top(U&& value) {
         assert(has_items());
-        m_data[m_top_index] = std::move(value);
+        m_data[m_top_index] = std::forward<U>(value);
         std::make_heap(m_data.data(), m_data.data() + m_size);
+    }
+
+    // Push new item is capacity allows it
+    // When queue is full the new item will replace worst item in the collection
+    // Worst item is the largest one
+    template<typename U>
+    void try_push(U&& value) {
+        if (is_full()) {
+            if (top() > value) {
+                replace_top(std::forward<U>(value));
+            }
+        }else{
+            push(std::forward<U>(value));
+        }
+    }
+
+    // queue must be empty
+    template<typename Iter>
+    void populate_from_collection(Iter begin, Iter end) {
+        assert(empty());
+        size_t input_size = std::distance(begin, end);
+
+        // First chunk of the input can fit in the queue
+        auto first_chunk_size = std::min(input_size, capacity());
+        m_data.assign(begin, begin + first_chunk_size);
+        m_size = first_chunk_size;
+        std::make_heap(m_data.data(), m_data.data() + m_size);
+
+        // Process the rest of the input
+        // Keep only the largest items
+        for (auto i = first_chunk_size; i < input_size; i++) {
+            try_push(*(begin + i));
+        }
     }
 
     bool empty() const {
--- priority_queue_tests.cpp
+++ priority_queue_tests.cpp
@@ -88,3 +88,184 @@ TEST(priority_queue, push_pop_replace_top_item) {
     queue.pop();
     ASSERT_TRUE(queue.empty());
 }
+
+TEST(priority_queue, populate_queue_from_collection) {
+    priority_queue<int> queue(10);
+    // shuffle the collection
+    std::vector<int> collection = {1, 7, 3, 10, 5, 6, 2, 8, 9, 4};
+    queue.populate_from_collection(collection.begin(), collection.end());
+    ASSERT_EQ(10, queue.size());
+    ASSERT_EQ(10, queue.top()) << "top item is largest";
+}
+
+TEST(priority_queue, populate_queue_from_large_collection) {
+    priority_queue<int> queue(5); // keep 5 smallest elements
+    // shuffle the collection
+    std::vector<int> collection = {1, 7, 3, 9, 5, 6, 2, 8, 10, 4};
+    queue.populate_from_collection(collection.begin(), collection.end());
+    ASSERT_EQ(5, queue.size());
+    ASSERT_EQ(5, queue.top()) << "keep 5 smallest elements, largest of them is 5";
+    queue.pop();
+    ASSERT_EQ(4, queue.size());
+    ASSERT_EQ(4, queue.top()) << "second largest item is 4";
+}
+
+TEST(priority_queue, pop_randomized){
+    priority_queue<int> queue(10);
+    // shuffle the collection
+    for (int x: {1, 7, 3, 10, 5, 6, 2, 8, 9, 4}) {
+        queue.push(x);
+    }
+    ASSERT_EQ(10, queue.size());
+    ASSERT_EQ(10, queue.top()) << "top item is largest";
+    queue.pop();
+    ASSERT_EQ(9, queue.size());
+    ASSERT_EQ(9, queue.top()) << "second largest item is 9";
+    queue.pop();
+    ASSERT_EQ(8, queue.size());
+    ASSERT_EQ(8, queue.top()) << "third largest item is 8";
+    queue.pop();
+    ASSERT_EQ(7, queue.size());
+    ASSERT_EQ(7, queue.top()) << "fourth largest item is 7";
+    queue.pop();
+    ASSERT_EQ(6, queue.size());
+    ASSERT_EQ(6, queue.top()) << "fifth largest item is 6";
+    queue.pop();
+    ASSERT_EQ(5, queue.size());
+    ASSERT_EQ(5, queue.top()) << "sixth largest item is 5";
+    queue.pop();
+    ASSERT_EQ(4, queue.size());
+    ASSERT_EQ(4, queue.top()) << "seventh largest item is 4";
+    queue.pop();
+    ASSERT_EQ(3, queue.size());
+    ASSERT_EQ(3, queue.top()) << "eighth largest item is 3";
+    queue.pop();
+    ASSERT_EQ(2, queue.size());
+    ASSERT_EQ(2, queue.top()) << "ninth largest item is 2";
+    queue.pop();
+    ASSERT_EQ(1, queue.size());
+    ASSERT_EQ(1, queue.top()) << "tenth largest item is 1";
+    queue.pop();
+}
+
+
+TEST(priority_queue, pop){
+    priority_queue<int> queue(10);
+    // Add elements from 1 to 10 in random order
+
+
+    queue.push(1);
+    queue.push(2);
+    queue.push(3);
+    queue.push(4);
+    queue.push(5);
+    queue.push(6);
+    queue.push(7);
+    queue.push(8);
+    queue.push(9);
+    queue.push(10);
+    ASSERT_EQ(10, queue.size());
+    ASSERT_EQ(10, queue.top()) << "top item is largest";
+    queue.pop();
+    ASSERT_EQ(9, queue.size());
+    ASSERT_EQ(9, queue.top()) << "second largest item is 9";
+    queue.pop();
+    ASSERT_EQ(8, queue.size());
+    ASSERT_EQ(8, queue.top()) << "third largest item is 8";
+    queue.pop();
+    ASSERT_EQ(7, queue.size());
+    ASSERT_EQ(7, queue.top()) << "fourth largest item is 7";
+    queue.pop();
+    ASSERT_EQ(6, queue.size());
+    ASSERT_EQ(6, queue.top()) << "fifth largest item is 6";
+    queue.pop();
+    ASSERT_EQ(5, queue.size());
+    ASSERT_EQ(5, queue.top()) << "sixth largest item is 5";
+    queue.pop();
+    ASSERT_EQ(4, queue.size());
+    ASSERT_EQ(4, queue.top()) << "seventh largest item is 4";
+    queue.pop();
+    ASSERT_EQ(3, queue.size());
+    ASSERT_EQ(3, queue.top()) << "eighth largest item is 3";
+    queue.pop();
+    ASSERT_EQ(2, queue.size());
+    ASSERT_EQ(2, queue.top()) << "ninth largest item is 2";
+    queue.pop();
+    ASSERT_EQ(1, queue.size());
+    ASSERT_EQ(1, queue.top()) << "tenth largest item is 1";
+}
+
+TEST(priority_queue, populate_queue_from_empty_collection) {
+    priority_queue<int> queue(10);
+    // shuffle the collection
+    std::vector<int> collection;
+    queue.populate_from_collection(collection.begin(), collection.end());
+    ASSERT_TRUE(queue.empty());
+}
+
+TEST(priority_queue, populate_queue_from_collection_with_duplicates) {
+    priority_queue<int> queue(10);
+    // shuffle the collection
+    std::vector<int> collection = {1, 3, 10, 5, 6, 2, 8, 9, 4, 10};
+    queue.populate_from_collection(collection.begin(), collection.end());
+    ASSERT_EQ(10, queue.size());
+    ASSERT_EQ(10, queue.top()) << "top item is largest";
+    queue.pop();
+    ASSERT_EQ(9, queue.size());
+    ASSERT_EQ(10, queue.top()) << "second largest item is 10 too";
+    queue.pop();
+    ASSERT_EQ(8, queue.size());
+    ASSERT_EQ(9, queue.top()) << "third largest item is 9";
+}
+
+TEST(priority_queue, try_push_one_item_to_empty_queue) {
+    priority_queue<int> queue(10);
+    queue.try_push(1);
+    ASSERT_EQ(queue.size(), 1);
+    ASSERT_EQ(queue.top(), 1);
+}
+
+TEST(priority_queue, try_push_one_item_to_full_queue) {
+    priority_queue<int> queue(1);
+    queue.push(1);
+    queue.try_push(2);
+    ASSERT_EQ(queue.size(), 1);
+    ASSERT_EQ(queue.top(), 1);
+}
+
+TEST(priority_queue, try_push_one_item_to_full_queue_item_is_replaced) {
+    priority_queue<int> queue(1);
+    queue.push(2);
+    queue.try_push(1);
+    ASSERT_EQ(queue.size(), 1);
+    ASSERT_EQ(queue.top(), 1);
+}
+TEST(priority_queue, try_push_multiple_items) {
+    // keep track of 3three smallest elements
+    priority_queue<int> queue(3);
+    queue.try_push(10);
+    ASSERT_EQ(queue.size(), 1);
+    ASSERT_EQ(queue.top(), 10);
+    queue.try_push(12);
+    ASSERT_EQ(queue.size(), 2);
+    ASSERT_EQ(queue.top(), 12);
+    queue.try_push(14);
+    ASSERT_EQ(queue.size(), 3);
+    ASSERT_EQ(queue.top(), 14);
+
+    queue.try_push(16);
+    ASSERT_EQ(queue.size(), 3); // element 16 is ignored
+    ASSERT_EQ(queue.top(), 14);
+
+    queue.try_push(11);
+    ASSERT_EQ(queue.size(), 3); // element 11 replace 14
+    ASSERT_EQ(queue.top(), 12);
+
+    queue.pop();
+    ASSERT_EQ(queue.size(), 2);
+    ASSERT_EQ(queue.top(), 11);
+
+    queue.pop();
+    ASSERT_EQ(queue.size(), 1);
+    ASSERT_EQ(queue.top(), 10);
+}
